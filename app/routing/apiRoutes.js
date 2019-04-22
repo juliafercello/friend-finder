7b40c0b3531212fad52475adf41ca093320540aa
Julia F <46096629+juliafercello@users.noreply.github.com>
2019-04-21 22:40:01 -0500

    draft of survey.html and enhance post route to find a match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,9 +10,33 @@ module.exports = function(app) {
 
   
   app.post("/api/friends", function(req, res) {
-      //FIND MATCH
-      friendData.push(req.body);
-      res.json(req.body);
+      var friend = req.body; 
+      var matchingScore = 0; 
+      var matchingfriend; 
+      var totalDifference = 0; 
+
+      //for each friend, compare the scores
+      for (var i = 0; i<friendData.length; i++){
+        console.log("checking match for " + friendData[i].name)
+        for(var j = 0; j<friend.scores.length; j++) {
+          var difference = parseInt(friend.scores[j]) - parseInt(friendData[i].scores[j]); 
+          // console.log(friend.scores[j]);
+          // console.log(friendData[i].scores[j]);
+          // console.log(difference)
+
+          totalDifference += Math.abs(difference);
+          console.log(totalDifference)
+        } 
+        //Return the first lowest match 
+        if (totalDifference < matchingScore || matchingScore === 0) {
+          matchingScore = totalDifference; 
+          matchingfriend = friendData[i]; 
+        }    
+        totalDifference = 0;     
+      }
+      friendData.push(req.body); 
+      res.json(matchingfriend);
+      console.log(matchingfriend);
   });
 
 //   app.post("/api/deletefriends", function(req, res) {
